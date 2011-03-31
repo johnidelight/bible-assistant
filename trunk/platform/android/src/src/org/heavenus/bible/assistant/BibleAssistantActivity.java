fb0fb0b341d58f4d03724e4f2eae20ad616f04d6
johnidelight@gmail.com <johnidelight@gmail.com@7d7fa329-27a9-a196-df5f-993b3572cee8>
2011-03-31 09:41:46 +0000

    the basic framework.

--- trunk/platform/android/src/src/org/heavenus/bible/assistant/BibleAssistantActivity.java
+++ trunk/platform/android/src/src/org/heavenus/bible/assistant/BibleAssistantActivity.java
@@ -1,13 +1,36 @@
 package org.heavenus.bible.assistant;
 
-import android.app.Activity;
+import android.app.ActivityGroup;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public class BibleAssistantActivity extends Activity {
-    /** Called when the activity is first created. */
+public class BibleAssistantActivity extends ActivityGroup {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
+        
+        ViewGroup rootLayout = (ViewGroup) findViewById(R.id.root_layout);
+        
+        // Add book list view.
+        Intent it = new Intent(this, BookListActivity.class);
+        View view = getLocalActivityManager().startActivity(BookListActivity.class.getName(), it)
+        		.getDecorView();
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.FILL_PARENT);
+        lp.weight = 1;
+        rootLayout.addView(view, lp);
+        
+        // Add book content view.
+        it = new Intent(this, BookContentActivity.class);
+        view = getLocalActivityManager().startActivity(BookContentActivity.class.getName(), it)
+        		.getDecorView();
+        lp = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.FILL_PARENT);
+        lp.weight = 3;
+        rootLayout.addView(view, lp);
     }
 }
